f71c4e93b4fc032a532a08cbe7fd1b5c5091894a
Jeroen P. Broks <facebook@tbbs.nl>
2018-10-04 16:18:35 +0200

    Position correction

--- clientside/main.js
+++ clientside/main.js
@@ -1,5 +1,1 @@
 // core code
-const ObjectId=(datatable)=>{
-  // TODO: Generate unique number based on the requested table!
-  return 1;
-} // temp!
--- serverside/main.js
+++ serverside/main.js
@@ -1,1 +1,5 @@
 // core code
+const ObjectId=(datatable)=>{
+  // TODO: Generate unique number based on the requested table!
+  return 1;
+} // temp!
