345395dc497da02d2f30b08cc3a18920cbccfee7
Jeroen P. Broks <facebook@tbbs.nl>
2018-10-04 16:07:14 +0200

    ObjectID

--- clientside/class_table.js
+++ clientside/class_table.js
@@ -1,15 +1,16 @@
-const new_id=()=>{return 1;} // temp!
+"use strict";
+
 
 
 class Table{
   constructor(data){
-     this.id=new_id("table");
+     this.id=ObjectID("table");
      this.maxchairs=data.maxchairs || 4; // if not set 4 will be used by default
      this.reservations=[]; // contains guest numbers. TODO: replace array with direct database readout over time!
      this.orders=[]; // link to orders ??
   }
 
-  function addguest(guestid){
+  addguest(guestid){
     this.reservations.push(guestid);
     // TODO: check if a number if present and if the guest_id exists at all!
     // Link can provide guest information!
