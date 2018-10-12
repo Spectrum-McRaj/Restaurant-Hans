79e6f9a8ec0a630567c4d6940caddc6dc4397a74
Jeroen P. Broks <facebook@tbbs.nl>
2018-10-12 15:13:06 +0200

    Table by minimal amount of chairs

--- serverside/class_table.js
+++ serverside/class_table.js
@@ -4,7 +4,8 @@
 
 class Table{
   constructor(data){
-     this.id=ObjectID("table");
+     const ObjectID=require("./ObjectID");
+     this.id=ObjectID(this,"table");
      this.maxchairs=data.maxchairs || 4; // if not set 4 will be used by default
      this.reservations=[]; // contains guest numbers. TODO: replace array with direct database readout over time!
      this.orders=[]; // link to orders ??
@@ -15,4 +16,15 @@ class Table{
     // TODO: check if a number if present and if the guest_id exists at all!
     // Link can provide guest information!
   }
+
+  static lookupbyminchairs(minchairs){
+    let ret = [];
+    let data = require("./temp_database");
+    for (let entry of data.table) {
+      if (entry.maxchairs>=minchairs) ret.push(entry);
+    }
+    return ret;
+  }
 }
+
+if (module) module.exports = Table;
--- serverside/main.js
+++ serverside/main.js
@@ -1,5 +1,16 @@
 // core code
-const ObjectId=(datatable)=>{
-  // TODO: Generate unique number based on the requested table!
-  return 1;
-} // temp!
+const temp_database = require("./temp_database");
+const ObjectId=require("./ObjectID.js");
+
+
+// /* test
+const tafel=require("./class_table");
+let t=[];
+for (i=0;i<=20;i++) t.push(new tafel({maxchairs:Math.ceil(Math.random()*10)}));
+console.log("t");
+console.log(t);
+console.log("\n\ndatabase");
+console.log(temp_database);
+console.log("\n\nMinimaal 6 stoelen")
+console.log(tafel.lookupbyminchairs(6));
+//*/
