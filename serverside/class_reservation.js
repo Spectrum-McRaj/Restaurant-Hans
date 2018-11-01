fd0c66f8e62572636a8b66a14ada06eb834976fe
Christoffel <ccolombijn@gmail.com>
2018-11-01 15:01:55 +0100

    Class Reservation

--- serverside/class_reservation.js
+++ serverside/class_reservation.js
@@ -1,21 +1,15 @@
 "use strict";
 
 class Reservation {
-  constructor (id, date, guest, persons, table, arrangement, comments) {
+  constructor (id, timestamp, guest, persons, table, arrangement, comments) {
 
     this._id = id;
-    this._date = date;
+    this._timestamp = timestamp;
     this._guest = guest;
     this._persons = persons;
     this._table = table;
     this._arrangement = arrangement;
     this._comments = comments;
-
-  }
-
-  get date () {
-    let date = new Date(this._date);
-    
 
   }
 
