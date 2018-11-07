51bf6ea4a729cd144f3ef62125f5098c36d1060c
ccolombijn <ccolombijn@gmail.com>
2018-11-07 18:23:32 +0100

    Christoffel : Added parameters/object switch & getPropertyLabels to class Ingredient

--- ingredient/ingredient.js
+++ ingredient/ingredient.js
@@ -2,15 +2,24 @@
 
 class Ingredient{
   constructor( id, name, price, inStock, unit, allergies, type){
-    Object.assign({id, name, price, inStock, unit, allergies, type}); // arguments als object
-    this.id = data.id;
-    this.name = data.name;
-    this.price = data.price;
-    this.inStock = data.inStock;
-    this.unit = data.unit;
-    this.allergies = data.allergies;
-    this.type = data.type;
-
+    if( typeof id === 'object'){ // arguments als object
+      let data = id;
+      this.id = data.id;
+      this.name = data.name;
+      this.price = data.price;
+      this.inStock = data.inStock;
+      this.unit = data.unit;
+      this.allergies = data.allergies;
+      this.type = data.type;
+    } else {
+      this.id = id;
+      this.name = name;
+      this.price = price;
+      this.inStock = inStock;
+      this.unit = unit;
+      this.allergies = allergies;
+      this.type = type;
+    }
   }
   used(amount){
     this.inStock -= amount;
