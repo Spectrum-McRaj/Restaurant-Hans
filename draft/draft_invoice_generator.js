b9dc5970fc38f25c74ec0ee138d99d51a762ae65
Jeroen P. Broks <facebook@tbbs.nl>
2018-11-09 16:24:12 +0100

    Fake data for invoice

---
+++ draft/draft_invoice_generator.js
@@ -0,0 +1,13 @@
+/* nep gegevens */
+let config = {
+     VAT:20 // VAT is de engelse term voor BTW. Ik ben even voor het gemak uitgegaan van 20%, dit kan later wellicht veranderd worden!
+}
+
+let dishes = { 1:{desc:"Lobster with kaviar", price: 50.00}, 2: {desc:"Bordeaux Wine", price: 24.50}, 3: {desc:"Dame Blanche with champagne", price: 30.25}}
+
+let reservation = {
+     guest:"Dagobert Duck",
+     items:[1,2,3],
+}
+
+/* einde nep gegevens */
