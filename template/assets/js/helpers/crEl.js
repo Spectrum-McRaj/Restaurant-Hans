4b8908991d5c9a06f1c1d01bda4666fd35ce1a04
Rob <rob@Mac-mini-G-IT.local>
2018-11-13 15:32:43 +0100

    Rob: updated CrEl tool

--- template/assets/js/helpers/crEl.js
+++ template/assets/js/helpers/crEl.js
@@ -2,26 +2,44 @@
 /*
 * assets/js/helpers/crEl.js
 */
-class crEl {
+class CrEl {
   constructor(rootdomelt) {
-    if (rootdomelt === null) {
+    if (rootdomelt === undefined) {
       this.parent = document.body;
     } else {
       this.parent = rootdomelt;  // { parent: <...> }
     }
   }
-  create(tag) { //Create Element
+  create(tag, upToParent) {
     let child = document.createElement(tag);
-    this.parent.appendChild(child);
-    let newEl = new crEl(child);
+    if (typeof upToParent == typeof true) {
+      this.parent.parentNode.appendChild(child);
+    } else {
+      this.parent.appendChild(child);
+    }
+    let newEl = new CrEl(child);
     return newEl;
   }
-  attr(name, value) { //Set Attribute
+  attr(name, value) {
     this.parent.setAttribute(name, value);
     return this;
   }
-  inner(content) { //Add inner HTML to element
+  inner(content) {
     this.parent.innerHTML = content;
     return this;
   }
+  act(element, type, listener) {
+    element.addEventListener(type, listener);
+  }
 }
+
+
+/* EXAMPLE USAGE:
+
+let div1 = new CrEl().create('div').attr('id','myWrapper');
+
+div1.create('div').attr('id','myContainer')
+      .create('h1').inner('Website titel')
+    .create('p',true).inner('Test').act(this,'click',(event)=>{console.log("Clicked")});
+
+*/
