a7cdac381a94745c9c414b8a283a4b6e779e5bc5
Jeroen P. Broks <facebook@tbbs.nl>
2018-11-15 16:06:30 +0100

    VAT is 10% for restaurant and hotels in Italy (22% is default VAT)

--- template/assets/js/modules/invoice_generator.js
+++ template/assets/js/modules/invoice_generator.js
@@ -1,6 +1,6 @@
 /* nep gegevens */
 let config = {
-     VAT:20 // VAT is de engelse term voor BTW. Ik ben even voor het gemak uitgegaan van 20%, dit kan later wellicht veranderd worden!
+     VAT:10 // VAT is de engelse term voor BTW. Ik ben even voor het gemak uitgegaan van 20%, dit kan later wellicht veranderd worden!
 }
 
 let dishes = { 1:{desc:"Lobster with kaviar", price: 50.00}, 2: {desc:"Bordeaux Wine", price: 24.50}, 3: {desc:"Dame Blanche with champagne", price: 30.25}, 4:{desc:"Mushroom soup", price:12}}
