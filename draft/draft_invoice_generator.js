0d5302cf42cebe112266a054e6cb4f7bfbac89aa
Jeroen P. Broks <facebook@tbbs.nl>
2018-11-15 14:42:32 +0100

    Print-friendly invoice

--- draft/draft_invoice_generator.js
+++ draft/draft_invoice_generator.js
@@ -3,32 +3,39 @@ let config = {
      VAT:20 // VAT is de engelse term voor BTW. Ik ben even voor het gemak uitgegaan van 20%, dit kan later wellicht veranderd worden!
 }
 
-let dishes = { 1:{desc:"Lobster with kaviar", price: 50.00}, 2: {desc:"Bordeaux Wine", price: 24.50}, 3: {desc:"Dame Blanche with champagne", price: 30.25}}
+let dishes = { 1:{desc:"Lobster with kaviar", price: 50.00}, 2: {desc:"Bordeaux Wine", price: 24.50}, 3: {desc:"Dame Blanche with champagne", price: 30.25}, 4:{desc:"Mushroom soup", price:12}}
 
 let reservation = {
      guest:"Dagobert Duck",
-     items:[1,2,3],
+     items:[4,1,2,3],
 }
 
 /* einde nep gegevens */
 
 
 function generate_invoice(process_reservation){
+  // NOTE: In Italy the euro sign comes BEHIND the price!!!
+  // The system could be enhanced to make it compatible with other countries
+  // Or other currencies.
   var total=0;
   var calcVAT;
-  var output="<table><caption><b>Invoice Molveno</b></caption>";
+  var output="<center><table><caption><b>Invoice Molveno</b></caption>";
+  output += `<tr><td colspan=2 align=right>Invoice for ${process_reservation.guest}`;
   for (let item of process_reservation.items)   {
       let dish=dishes[item]
       output += `<tr><td>${dish.desc}</td>`;
-      output +  "<td align=right>"+dish.price+"</td></tr>"; // TODO: Make sure this is in a xxxx.yy format
+      output += "<td align=right>"+Number(dish.price).toFixed(2)+" &euro;</td></tr>"; // TODO: Make sure this is in a xxxx.yy format
       total  += dish.price;
     }
     calcVAT  = total * (config.VAT/100);
     output   += "<tr><td></td><td><hr></td></tr>";
-    output   += `<tr><td align=right>Total excl. VAT:</td><td>${total}</td></tr>`;
-    output   += `<tr><td align=right>VAT ${config.VAT}%:</td><td>${calcVAT}</td></tr>`;
-    output   += `<tr><td align=right>Total incl. VAT:</td><td>${total-calcVAT}`;
+    output   += `<tr><td align=right>Total excl. VAT:</td><td align=right>${Number(total).toFixed(2)} &euro;</td></tr>`;
+    output   += `<tr><td align=right>VAT ${config.VAT}%:</td><td align=right>${Number(calcVAT).toFixed(2)} &euro;</td></tr>`;
+    output   += `<tr><td align=right>Total incl. VAT:</td><td align=right>${Number(total+calcVAT).toFixed(2)} &euro;</td></tr>`;
+    output   += "</table></center>";
     return output
 }
 
+/* node test!
 console.log(generate_invoice(reservation));
+//*/
